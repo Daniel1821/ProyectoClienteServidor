ebca7b1aa5ab94c2e659690f69606b8ede2e6531
Daniel1821 <danielbs2272@gmail.com>
2024-12-07 16:27:18 -0600

    Se agrega el mensaje en el main

--- src/main/java/com/ecodeup/jdbc/Main.java
+++ src/main/java/com/ecodeup/jdbc/Main.java
@@ -5,6 +5,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esto es un mensaje nuevo");
         ConexionBD conexion = new ConexionBD();
         conexion.consultarTablas();
     }
